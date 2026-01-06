e74b78773e3b4230ef3c6151f219497099245d69
sajidwebz786 <167985844+sajidwebz786@users.noreply.github.com>
2026-01-06 05:54:48 +0530

    update
    update

--- models/index.js
+++ models/index.js
@@ -1,34 +1,64 @@
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const config = require(path.join(__dirname, '/../config/config.json'))[env];
 
+const db = {};
 let sequelize;
+
+/**
+ * Initialize Sequelize
+ */
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
+/**
+ * Load all models
+ */
+fs.readdirSync(__dirname)
+  .filter(file =>
+    file.indexOf('.') !== 0 &&
+    file !== basename &&
+    file.slice(-3) === '.js'
+  )
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const modelFactory = require(path.join(__dirname, file));
+
+    // ✅ SAFETY CHECK
+    if (typeof modelFactory !== 'function') {
+      throw new Error(
+        `Model file ${file} does not export a function (sequelize, DataTypes)`
+      );
+    }
+
+    const model = modelFactory(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
+/**
+ * Run associations
+ */
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
+/**
+ * Export
+ */
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
