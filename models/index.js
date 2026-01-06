07df7d23053c4ff47b8df2db66d320a5c76a26a7
sajidwebz786 <167985844+sajidwebz786@users.noreply.github.com>
2026-01-06 05:46:15 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -19,16 +19,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const modelModule = require(path.join(__dirname, file));
-    let model;
-    if (modelModule.init) {
-      // Class-based model
-      modelModule.init(sequelize);
-      model = modelModule;
-    } else {
-      // Function-based model
-      model = modelModule(sequelize, Sequelize.DataTypes);
-    }
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
