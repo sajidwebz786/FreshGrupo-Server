bef9dd32c1e51e95d4bf2d189c9ef7fede0830ae
sajidwebz786 <sajidwebz@gmail.com>
2026-03-21 11:53:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -591,7 +591,7 @@ const upload = multer({ storage: storage });
         const packProducts = await PackProduct.findAll({
           where: { packId },
           include: [
-            { model: UnitType }
+            { model: UnitType, as: 'UnitType' }
           ]
         });
 
